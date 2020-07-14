366597f69f46e08507a1e93849f21b79a8eec271
Mr-Thunder <ruairi_thunder@hotmail.com>
2020-07-14 22:55:23 +0100

    Added loading animation

--- src/js/global.js
+++ src/js/global.js
@@ -1,5 +1,8 @@
 console.log("hello there yo");
+
+// Loading Animation
 window.addEventListener("load", function () {
     loader = document.querySelector(".loader");
     loader.className += " hidden";
+    
 });
